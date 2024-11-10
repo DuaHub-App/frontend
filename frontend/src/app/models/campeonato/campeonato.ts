60b802f537cfbf6932c28354cfa7528cfdf7d384
hoesel15 <jvhoesel@live.com>
2024-11-10 19:21:10 -0300

    Configurando Models by Ribeiro

--- frontend/src/app/models/campeonato/campeonato.ts
+++ frontend/src/app/models/campeonato/campeonato.ts
@@ -1,9 +1,16 @@
 export class Campeonato {
-    
-    
-    id!: number;
-    nome!: string;
-    participantes!: string;
-    campeonato!: string; 
+    id?: number;
+    nome: string = '';
+    participante: string = '';
+    campeonato: string = '';
 
+    constructor(
+    nome: string = '',
+    participante: string = '',
+    campeonato: string = '',
+    ) {
+        this.nome = nome;
+        this.participante = participante;
+        this.campeonato = campeonato;
+}
 }
--- frontend/src/app/models/equipe/equipe.ts
+++ frontend/src/app/models/equipe/equipe.ts
@@ -1,8 +1,16 @@
-export class Equipe { 
-    
-        id!: number;
-        nome!: string;
-        participantes!: string;
-    
-    }
-    
+export class Equipe {
+    id?: number;
+    nome: string = '';
+    equipe: string = '';
+    participante: any[] = []
+
+    constructor(
+    nome: string = '',
+    equipe: string = '',
+    participante: any[] = []
+    ) {
+        this.nome = nome;
+        this.equipe = equipe,
+        this.participante = [] = []
+}
+}
