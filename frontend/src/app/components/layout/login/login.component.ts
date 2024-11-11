5d69191404b352572c18ff847a669fc09670488a
lfg-Ribeiro <luanfp2004@hotmail.com>
2024-11-11 17:00:03 -0300

    ajustado os alertas da aplicação com sweetAlert e realizado a melhoria da estilização do site

--- frontend/src/app/components/layout/login/login.component.ts
+++ frontend/src/app/components/layout/login/login.component.ts
@@ -2,27 +2,46 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
-
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [MdbFormsModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-
   usuario!: string;
   senha!: string;
 
   router = inject(Router);
 
+  logar() {
+    const usuariosValidos = [
+      { usuario: 'admin', senha: 'admin' },
+      { usuario: 'hoesel', senha: 'hoesel' },
+      { usuario: 'ribeiro', senha: 'ribeiro' },
+      { usuario: 'tchola', senha: 'tchola' },
+    ];
 
-  logar(){
-    if(this.usuario =='admin' && this.senha =='admin'){
-      this.router.navigate(['/menu'])
-    }else
-    alert('Usuario ou senha esta incorreto!');
+    const credencialValida = usuariosValidos.some(
+      (credencial) =>
+        credencial.usuario === this.usuario && credencial.senha === this.senha
+    );
+
+    if (credencialValida) {
+      Swal.fire({
+        title: 'Acesso Permitido!',
+        icon: 'success',
+      });
+
+      this.router.navigate(['/menu']);
+    } else {
+      Swal.fire({
+        title: 'Usuário ou senha incorretos!',
+        icon: 'error',
+      });
+    }
   }
 }
