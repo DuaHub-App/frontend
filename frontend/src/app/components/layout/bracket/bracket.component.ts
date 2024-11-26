4ede07b66bcdac015ebb5b6b8ee8b893b3d4da9c
lfg-Ribeiro <luanfp2004@hotmail.com>
2024-11-26 17:52:26 -0300

    atualiza estilos e estrutura do componente de chaveamento; adiciona modal para exibição de dados de classificação

--- frontend/src/app/components/layout/bracket/bracket.component.ts
+++ frontend/src/app/components/layout/bracket/bracket.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-bracket',
@@ -9,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./bracket.component.scss'],
 })
 export class BracketComponent {
-  
+  isModalOpen: boolean = false; // Controla a visibilidade do modal
+  classificationData = [
+    { name: 'URSO', points: 12 },
+    { name: 'JAGUARES', points: 9 },
+    { name: 'QUATI', points: 7 },
+    { name: 'KORINGA', points: 5 },
+    { name: 'DOSE LETAL', points: 4 },
+    { name: 'TUCANOS', points: 3 },
+    { name: 'CARPINCHO', points: 2 },
+    { name: 'PORCO', points: 1 },
+    { name: 'MEDUSA', points: 0 },
+    { name: 'SOBERANA', points: 0 }
+    // Adicione mais dados conforme necessário
+  ];
+
+  openModal(): void {
+    this.isModalOpen = true;
+  }
+
+  closeModal(): void {
+    this.isModalOpen = false;
+  }
 }
