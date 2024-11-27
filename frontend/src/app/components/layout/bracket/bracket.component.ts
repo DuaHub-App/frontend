4178a27b599c1b20f2445fa7c144e5265aa6b2fb
lfg-Ribeiro <luanfp2004@hotmail.com>
2024-11-27 15:57:22 -0300

    remove arquivos de teste para componentes não utilizados

--- frontend/src/app/components/layout/bracket/bracket.component.ts
+++ frontend/src/app/components/layout/bracket/bracket.component.ts
@@ -11,16 +11,16 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class BracketComponent {
   isModalOpen: boolean = false; // Controla a visibilidade do modal
   classificationData = [
-    { name: 'URSO', points: 12 },
-    { name: 'JAGUARES', points: 9 },
-    { name: 'QUATI', points: 7 },
-    { name: 'KORINGA', points: 5 },
-    { name: 'DOSE LETAL', points: 4 },
-    { name: 'TUCANOS', points: 3 },
-    { name: 'CARPINCHO', points: 2 },
-    { name: 'PORCO', points: 1 },
-    { name: 'MEDUSA', points: 0 },
-    { name: 'SOBERANA', points: 0 }
+    { pos: '1º', name: 'UNIAMÉRICA', points: 12 },
+    { pos: '2º', name: 'MEDICINA UPE FRANCO', points: 9 },
+    { pos: '3º', name: 'ENG & ARQ UDC', points: 7 },
+    { pos: '4º', name: 'MEDICINA UPE CDE', points: 5 },
+    { pos: '5º', name: 'HUMANIDADES UNILA', points: 4 },
+    { pos: '6º', name: 'UNIGUAÇU', points: 3 },
+    { pos: '7º', name: 'ENG & ARQ UNILA', points: 2 },
+    { pos: '8º', name: 'UTFPR-MD', points: 1 },
+    { pos: '9º', name: 'MEDICINA UNILA', points: 0 },
+    { pos: '10º', name: 'MEDICINA UNINTER', points: 0 }
     // Adicione mais dados conforme necessário
   ];
 
