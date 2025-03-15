1cc9e8b6f2e13d5718fdcf53f2ca1eb427dd6d0f
ProjetoMensal <fvcominetti@gmail.com>
2025-03-15 18:24:30 -0300

    feat: Tela inicial, com novos cards de ultimo torneio e torneio em destaque (atual), corrigi bug de tabela de classificação

--- frontend/src/app/components/layout/qualifer-campeonato/qualifer-campeonato.component.ts
+++ frontend/src/app/components/layout/qualifer-campeonato/qualifer-campeonato.component.ts
@@ -22,76 +22,76 @@ export class QualiferCampeonatoComponent {
       icon: '/assets/images/easy/1.svg',
       modalidade: '1º Período',
       tipo: 'Turma A',
-      atleticas: 10,
+      numEquipes: 10,
       campeao: 'UNIAMÉRICA'
     },
     {
       icon: '/assets/images/easy/2.svg',
       modalidade: '1º Período',
       tipo: 'Turma B',
-      atleticas: 14,
+      numEquipes: 14,
       campeao: 'UNIAMÉRICA'
     },
     {
       modalidade: '1º Período',
       tipo: 'Turma C',
-      atleticas: 15,
+      numEquipes: 15,
       campeao: 'EXATAS UTFPR-MD',
       icon: '/assets/images/easy/3.svg'
     },
     {
       modalidade: '1º Período',
       tipo: 'Turma D',
-      atleticas: 15,
+      numEquipes: 15,
       campeao: 'UNIGUAÇU',
       icon: '/assets/images/easy/4.svg'
     },
     {
       modalidade: '1º Período',
       tipo: 'Turma E',
-      atleticas: 14,
+      numEquipes: 14,
       campeao: 'UNIGUAÇU',
       icon: '/assets/images/easy/5.svg'
     },
     {
       modalidade: '1º Período',
       tipo: 'Turma F',
-      atleticas: 14,
+      numEquipes: 14,
       campeao: 'UNIGUAÇU',
       icon: '/assets/images/easy/6.svg'
     },
     {
       modalidade: '1º Período',
       tipo: 'Turma G',
-      atleticas: 10,
+      numEquipes: 10,
       campeao: 'UNIGUAÇU',
       icon: '/assets/images/easy/7.svg'
     },
     {
       modalidade: '1º Período',
       tipo: 'Turma H',
-      atleticas: 14,
+      numEquipes: 14,
       campeao: 'UNIAMÉRICA',
       icon: '/assets/images/easy/8.svg'
     },
     {
       modalidade: '1º Período',
       tipo: 'Turma I',
-      atleticas: 14,
+      numEquipes: 14,
       campeao: 'UNIGUAÇU',
       icon: '/assets/images/easy/9.svg'
     },
     {
       modalidade: '1º Período',
       tipo: 'Turma J',
-      atleticas: 15,
+      numEquipes: 15,
       campeao: 'MEDICINA UPE CDE',
       icon: '/assets/images/easy/10.svg'
     },
     {
       modalidade: '1º Período',
       tipo: 'Turma K',
-      atleticas: 14,
+      numEquipes: 14,
       campeao: 'ENG E ARQ UDC',
       icon: '/assets/images/easy/1.svg'
     }
