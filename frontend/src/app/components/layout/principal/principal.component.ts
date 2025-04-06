4d216ec86ef4e87c4c5327cf51015ca63fb8baed
ProjetoMensal <fvcominetti@gmail.com>
2025-04-06 16:31:52 -0300

    feat & fix: código mais clean, sistema mais limpo e adicionado algumas telas necessárias para o funcionamento do sistema

--- frontend/src/app/components/layout/principal/principal.component.ts
+++ frontend/src/app/components/layout/principal/principal.component.ts
@@ -1,16 +1,34 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { MenuComponent } from '../menu/menu.component';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { CampeonatoComponent } from '../../campeonato/campeonato.component';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [CommonModule, MenuComponent, RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.scss'
 })
-export class PrincipalComponent {
+export class PrincipalComponent implements OnInit {
+  isSidebarClosed = false;
+  rotaAtual: string = "";
 
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.rotaAtual =  this.router.url;
+  }
+
+  toggleSidebar() {
+    this.isSidebarClosed = !this.isSidebarClosed;
+  }
+  sair() {
+    this.router.navigate(['/login']);
+  }
+
+  navegarEntreRotas(rota: string) {
+    this.router.navigate([`/admin/${rota}`]);
+    this.rotaAtual = `/${rota}`;
+  }
 }
